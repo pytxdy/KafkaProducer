032f3b7fe2791762c284cee5236e68ca0e3c6cd1
yang_pei <1024090119@qq.com>
2022-07-15 19:22:15 +0800

    123

--- src/main/java/com/py/KafkaProducer/controller/SendMessageController.java
+++ src/main/java/com/py/KafkaProducer/controller/SendMessageController.java
@@ -4,8 +4,12 @@ package com.py.KafkaProducer.controller;
 import com.py.KafkaProducer.kafka.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class SendMessageController {
@@ -14,8 +18,9 @@ public class SendMessageController {
     @Autowired
     Producer producer;
 
-    @PostMapping("/send")
+    @GetMapping("/send")
     public String sendMessage(String message) {
+        System.out.println(message);
         producer.sendHighPriorityMessage(message);
         return "ok";
     }
--- src/main/java/com/py/KafkaProducer/kafka/Producer.java
+++ src/main/java/com/py/KafkaProducer/kafka/Producer.java
@@ -2,21 +2,30 @@ package com.py.KafkaProducer.kafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class Producer {
 
+
+    public static final String highPriorityTopic = "highTest";
+
+    public static final String mediumPriorityTopic = "mediumTest";
+
+    public static final String lowPriorityTopic = "lowTest";
     @Autowired
     public KafkaTemplate kafkaTemplate;
 
     public void sendHighPriorityMessage(String msg) {
-        kafkaTemplate.send("test", 0, msg);
+        kafkaTemplate.send(highPriorityTopic, msg);
     }
 
     public void sendMediumPriorityMessage(String msg) {
-        kafkaTemplate.send("test", 1, msg);
+        kafkaTemplate.send(mediumPriorityTopic, msg);
     }
 
     public void sendLowPriorityMessage(String msg) {
-        kafkaTemplate.send("test", 2, msg);
+        kafkaTemplate.send(lowPriorityTopic, msg);
     }
 }
